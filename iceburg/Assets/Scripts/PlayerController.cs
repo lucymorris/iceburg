bfa5cc664a68d09fafa55d72f1174433313bc4aa
lucy <morris.lucyann@gmail.com>
2017-07-02 21:04:23 +0200

    flip flop, bitches
    player movement/animation triggers working, half of a camera script
    finished - need to attach to player prefab

---
+++ iceburg/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	static Animator animator;
+	public float speed = 2.0f;
+	public float rotationSpeed = 150.0f;
+
+	void Start () {
+
+		animator = GetComponent<Animator> ();
+
+	}
+	
+	void Update () {
+
+		float translation = Input.GetAxis ("Vertical") * speed;
+		float rotation = Input.GetAxis ("Horizontal") * rotationSpeed;
+		translation *= Time.deltaTime;
+		rotation *= Time.deltaTime;
+
+		Debug.LogFormat ("Rotation is {0} is  speed {1} times {2}", rotation, rotationSpeed, Input.GetAxis ("Horizontal"));
+
+		transform.Translate (0, 0, translation);
+		transform.Rotate (0, rotation, 0);
+
+		if (translation != 0) {
+			animator.SetBool ("IsWalking", true);
+		} else {
+			animator.SetBool ("IsWalking", false);
+		}
+			
+
+		// player facing camera direction
+
+	
+}
+}
---
+++ iceburg/Assets/Scripts/ThirdPersonCameraController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCameraController : MonoBehaviour {
+
+	public Transform playerCam, character, centrePoint;
+
+	private float mouseX, mouseY;
+	public float mouseSensitivity = 10f;
+
+	private float moveFB, moveLR;
+	public float moveSpeed = 2f;
+
+	private float zoom;
+	public float zoomSpeed = 2;
+
+	public float zoomMin = -2f;
+	public float zoomMax = -10f;
+
+	// Use this for initialization
+	void Start () {
+
+		zoom = -1;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		zoom += Input.GetAxis ("Mouse ScrollWheel") * zoomSpeed;
+
+		if (zoom > zoomMin)
+			zoom = zoomMin;
+
+		if (zoom < zoomMax)
+			zoom = zoomMax;
+
+		playerCam.transform.position = new Vector3 (playerCam.transform.position.x, playerCam.transform.position.y, zoom);
+
+		if (Input.GetMouseButton (1)) {
+			mouseX += Input.GetAxis ("Mouse X");
+			mouseY -= Input.GetAxis ("Mouse Y");
+		}
+
+		mouseY = Mathf.Clamp (mouseY, -60f, 60f);
+		playerCam.LookAt (centrePoint); 
+		centrePoint.localRotation = Quaternion.Euler (mouseY, mouseX, 0);
+
+
+	}
+}
