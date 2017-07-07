61aed33a4d7cc32dd1f34a45e06d43068d21af19
lucy <morris.lucyann@gmail.com>
2017-07-07 18:16:43 +0200

    spawn/despawn scripts for env consumables, skybox test
    basic spawn/despawn, needs tweaking so pickup only happens on interact
    with key, not on mesh collision

---
+++ iceburg/Assets/Scripts/environment scripts/respawn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class respawn : MonoBehaviour {
+
+    void OnCollisionEnter()
+    {
+        this.GetComponent<SphereCollider>().enabled = false;
+        this.GetComponent<MeshRenderer>().enabled = false;
+
+        Invoke("Respawn", 5);
+    }
+
+    void Respawn()
+    {
+        this.GetComponent<SphereCollider>().enabled = true;
+        this.GetComponent<MeshRenderer>().enabled = true;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ iceburg/Assets/Scripts/environment scripts/spawnConsumable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnConsumable : MonoBehaviour {
+
+    public GameObject fruit;
+    int spawnNum = 4;
+
+	void spawn()
+    {
+        for(int i = 0; i < spawnNum; i++)
+        {
+            Vector3 fruitPos = new Vector3(this.transform.position.x + Random.Range(-0.1f, 0.15f),
+                                            this.transform.position.y + Random.Range(0.0f, 0.05f),
+                                            this.transform.position.z + Random.Range(-0.1f, 0.15f));
+            Instantiate(fruit,fruitPos, Quaternion.identity);
+        }
+    }
+
+	void Start () {
+        spawn();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
