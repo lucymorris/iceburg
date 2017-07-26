a12583f752c90cf0b468ee0e6ba9528335fc4363
Elliot Hayward <kezeali@gmail.com>
2017-07-26 23:39:06 +1200

    new player input stuff i forgot to add

---
+++ iceburg/Assets/Scripts/InputConfig.cs
@@ -0,0 +1,16 @@
+
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "InputConfig", menuName = "Config/InputConfig", order = 1)]
+public class InputConfig : ScriptableObject
+{
+  public string moveForwardBack = "Vertical";
+  public string moveRightLeft = "Horizontal";
+  public string camPan ="Mouse X";
+  public string camPitch ="Mouse Y";
+  public string camZoom ="Mouse ScrollWheel";
+  public string toggleZoom ="";
+  public bool useMouse = false;
+  public float camZoomSensitivity = 1;
+  public float camTurnSensitivity = 1;
+}
